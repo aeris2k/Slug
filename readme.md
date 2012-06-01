d66889ae3770f28364485238de7ef369a58b1698
aeris2k <m.f@inode.at>
2012-06-01 17:26:45 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+Usage example
+=============
+
+public function set_name($name)
+{
+	$this->set_attribute('name', $name);
+    $this->set_attribute('slug', Slug::create_unique($name, $this));
+}
---
+++ slug.php
@@ -0,0 +1,28 @@
+<?php
+
+class Slug
+{
+    public static function create_unique($string = '', $model, $field = 'slug')
+    {
+        $slug = Str::slug($string);
+
+        $id = $model->get_attribute('id');
+        if(null === $id) $id = 0;
+
+        $i = 0;
+
+        while($model->where($field, '=', $slug)->where('id', '!=', $id)->count() > 0)
+        {
+            if (!preg_match ('/-{1}[0-9]+$/', $slug ))
+            {
+               $slug .= '-' . ++$i;
+            }
+            else
+            {
+               $slug = preg_replace ('/[0-9]+$/', ++$i, $slug );
+            }
+        }
+
+        return $slug;
+    }
+}
